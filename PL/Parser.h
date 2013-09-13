14d76397f3a3cc69c08a468bca2af668b899889d
hnefatl <hnefatl@gmail.com>
2013-09-13 18:13:57 +0100

    Working

--- PL/Parser.h
+++ PL/Parser.h
@@ -1,10 +1,14 @@
 #ifndef _PARSER_H
 #define _PARSER_H
 
+#include "Sentence.h"
+
+#include <string>
+
 class Parser
 {
 public:
-
+	static Sentence Parse(std::string Input);
 
 };
 
--- PL/Sentence.h
+++ PL/Sentence.h
@@ -1,6 +1,10 @@
 #ifndef _SENTENCE_H
 #define _SENTENCE_H
 
-
+class Sentence
+{
+public:
+	Sentence();
+};
 
 #endif
